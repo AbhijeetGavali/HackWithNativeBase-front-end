f0b982194147628015084143f17209e77c7dc093
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2022-07-03 12:01:50 +0530

    registration

--- src/redux/action/user.js
+++ src/redux/action/user.js
@@ -1,5 +1,12 @@
 import axiosInstance from "../../../utils/axiosInstance";
-import { SIGNIN_FAIL, SIGNIN_START, SIGNIN_SUCCESS } from "./action.type";
+import {
+  SIGNIN_FAIL,
+  SIGNIN_START,
+  SIGNIN_SUCCESS,
+  SIGNUP_FAIL,
+  SIGNUP_START,
+  SIGNUP_SUCCESS,
+} from "./action.type";
 
 const signinStart = () => ({
   type: SIGNIN_START,
@@ -29,6 +36,38 @@ export const signin = (user, message, navigation) => {
         console.log(err);
         message(err.response?.data?.message);
         dispatch(signinFailed(err));
+      });
+  };
+};
+
+const signupStart = () => ({
+  type: SIGNUP_START,
+});
+
+const signupSuccess = (user) => ({
+  type: SIGNUP_SUCCESS,
+  payload: user,
+});
+
+const signupFailed = (error) => ({
+  type: SIGNUP_FAIL,
+  pyaload: error,
+});
+
+export const signup = (user, message, navigation) => {
+  return (dispatch) => {
+    dispatch(signupStart());
+    axiosInstance
+      .post("/signup", user)
+      .then((response) => {
+        dispatch(signupSuccess(response.data));
+        message("Signup Success");
+        navigation.navigate("MyTabs");
+      })
+      .catch((err) => {
+        console.log(err);
+        message(err.response?.data?.message);
+        dispatch(signupFailed(err));
       });
   };
 };
