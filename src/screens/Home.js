2b84bd148f38c2f1b670dfe9ed76925bab2dea06
Avishkar <friends4ever.avi@gmail.com>
2022-07-03 10:16:54 +0530

    conflits resolve

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,5 @@
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
-<<<<<<< HEAD
- import Card from '../components/Card'
-import Scanner from "../components/Scanner"
-import { VStack } from "native-base";
-const Home = () => {
-  return (
-    <Card></Card>
-    // <VStack flex={1}>
-  //   <Card/> 
-  //   <VStack bg="red.500" flex={1}/>
-    
-  // </VStack>
-    );
-=======
 import Card from "../components/Card";
 import Scanner from "../components/Scanner";
 const Home = ({ navigation }) => {
@@ -26,10 +12,10 @@ const Home = ({ navigation }) => {
       {/* <SafeAreaView>
         <Card />
       </SafeAreaView> */}
+
       <Scanner navigateToHome={navigateToHome} />
     </>
   );
->>>>>>> b535baa98f030a788545e6c2f62edd8229e14e2a
 };
 
 export default Home;
